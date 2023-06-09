696538a884453376cb7786e758ce2170307558bf
Semi Invader <=>
2023-06-09 16:26:01 -0600

    needed for testing

---
+++ blockchain/contracts/mock/MockERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+contract MockToken is ERC20PresetFixedSupply {
+    constructor()
+        ERC20PresetFixedSupply("MockToken", "MT", 1000000 ether, msg.sender)
+    {}
+}
