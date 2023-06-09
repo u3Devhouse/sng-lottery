8ba9a5ae4a6d5ff6825aba300b7ddbb5d865704b
Semi Invader <=>
2023-06-09 16:26:15 -0600

    added some more tests

--- blockchain/test/LotteryTests.ts
+++ blockchain/test/LotteryTests.ts
@@ -5,37 +5,83 @@ import { ethers } from "hardhat";
 import { convertToHex } from "./utils";
 import { parseEther } from "ethers/lib/utils";
 
-const usdtETH = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 
 describe("Lottery", function () {
 
   async function setup(){
     const [owner, user1, user2, user3, user4, user5] = await ethers.getSigners();
     const LotteryFactory = await ethers.getContractFactory("BlazeLottery", owner);
-    const lottery = await LotteryFactory.deploy(usdtETH)
-    const usdtWhale = await ethers.getImpersonatedSigner("0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503")
-    const usdt = await ethers.getContractAt("IERC20", usdtETH, usdtWhale)
-    await usdt.transfer(user1.address, ethers.utils.parseEther("1000"))
-    await usdt.transfer(user2.address, ethers.utils.parseEther("1000"))
-    await usdt.transfer(user3.address, ethers.utils.parseEther("1000"))
-    await usdt.transfer(user4.address, ethers.utils.parseEther("1000"))
-    return {lottery, owner, user1, user2, user3, user4, user5, usdt}
+    const MockTokenFactory = await ethers.getContractFactory("MockToken", owner);
+    const mockToken = await MockTokenFactory.deploy();
+    const lottery = await LotteryFactory.deploy(mockToken.address);
+    await mockToken.transfer(user1.address, ethers.utils.parseEther("1000"))
+    await mockToken.transfer(user2.address, ethers.utils.parseEther("1000"))
+    await mockToken.transfer(user3.address, ethers.utils.parseEther("1000"))
+    await mockToken.transfer(user4.address, ethers.utils.parseEther("1000"))
+    return {lottery, owner, user1, user2, user3, user4, user5, mockToken}
+  }
+
+  async function setupStarted () {
+    const init = await setup()
+    const {lottery, owner, user1, user2, user3, user4, user5, mockToken} = init
+    await lottery.activateLottery(parseEther("10"))
+    await mockToken.connect(user1).approve(lottery.address, parseEther("1000"))
+    await mockToken.connect(user2).approve(lottery.address, parseEther("1000"))
+    await mockToken.connect(user3).approve(lottery.address, parseEther("1000"))
+    await mockToken.connect(user4).approve(lottery.address, parseEther("1000"))
+    await mockToken.connect(user5).approve(lottery.address, parseEther("1000"))
+    return init
   }
 
   describe("Owner Functions", ()=>{
     it("Should set the price for current Round", async() => {
-      const { lottery, owner, user1, user2, user3, user4, user5, usdt} = await loadFixture(setup);
+      const { lottery, owner, user1, user2, user3, user4, user5, mockToken} = await loadFixture(setup);
       await expect(lottery.connect(user1).setPrice(0, 0)).to.be.revertedWith("Ownable: caller is not the owner");
-      await lottery.setPrice( parseEther("10"), 1, )
+      await lottery.setPrice( parseEther("12"), 1, )
       const round1Info = await lottery.roundInfo(1)
-      expect(round1Info.price).to.equal(parseEther("10"))
+      expect(round1Info.price).to.equal(parseEther("12"))
     })
 
   })
 
+  describe("Calculation functions", () => {
+    it("Should check that ticket matches are the same regardless of scenario", async () => {
+      const { lottery } = await loadFixture(setup);
+
+      const ticketWinner = convertToHex([10,20,30,40,50])
+      
+      let ticketToBuy = convertToHex([10,20,30,40,50])
+      expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(5)
+      
+      ticketToBuy = convertToHex([50,40,30,20,10])
+      expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(5)
+
+      ticketToBuy = convertToHex([10,20,30,40,60])
+      expect(await lottery.checkTicketMatching(ticketToBuy, ticketWinner)).to.equal(4)
+      expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(4)
+      
+      ticketToBuy = convertToHex([88,1,0,3,0])
+      expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(0)
+
+      // TODO THIS PARTICULAR CASE IS STILL PENDING
+      ticketToBuy = convertToHex([1,40,63, 74, 148 ])
+      expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(3)
+    })
+  })
+
   describe( "Buy Tickets", function (){
-    it("should buy 2 tickets", async function (){
-      const { lottery, owner, user1, user2, user3, user4, user5} = await loadFixture(setup);
+    it("Should not allow to buy tickets until the round is started", async ()=>{
+      const { lottery, user1 } = await loadFixture(setup);
+      const ticketToBuy = convertToHex([10,20,30,40,50])
+      await expect(lottery.connect(user1).buyTickets(new Array(10).fill(ticketToBuy))).to.be.revertedWithCustomError(lottery,"RoundInactive").withArgs(0)
+    })
+    it("Should not allow to buy tickets until the round is started NOT EVEN OWNER", async ()=>{
+      const { lottery, owner } = await loadFixture(setup);
+      const ticketToBuy = convertToHex([10,20,30,40,50])
+      await expect(lottery.connect(owner).buyTickets(new Array(10).fill(ticketToBuy))).to.be.revertedWithCustomError(lottery,"RoundInactive").withArgs(0)
+    })
+    it("should buy tickets", async function (){
+      const { lottery, owner, user1, user2, user3, user4, user5} = await loadFixture(setupStarted);
       const ticketToBuy = convertToHex([10,20,30,40,50])
       const user2TicketsToBuy = new Array(100).fill(ticketToBuy)
       await lottery.connect(user1).buyTickets(new Array(10).fill(ticketToBuy))
