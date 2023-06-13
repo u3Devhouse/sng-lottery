1d27dbca36aaa50c9eaccffaa8b0cd6a7066e01d
Semi Invader <=>
2023-06-13 16:54:04 -0600

    add to pot

--- blockchain/test/LotteryTests.ts
+++ blockchain/test/LotteryTests.ts
@@ -9,7 +9,7 @@ import { parseEther } from "ethers/lib/utils";
 describe("Lottery", function () {
 
   async function setup(){
-    const [owner, user1, user2, user3, user4, user5, team] = await ethers.getSigners();
+    const [owner, user1, user2, user3, user4, user5, team, upkeep] = await ethers.getSigners();
     const LotteryFactory = await ethers.getContractFactory("BlazeLottery", owner);
     const MockTokenFactory = await ethers.getContractFactory("MockToken", owner);
     const MockVRFFactory = await ethers.getContractFactory("VRFCoordinatorV2Mock", owner);
@@ -23,13 +23,14 @@ describe("Lottery", function () {
     await mockToken.transfer(user3.address, ethers.utils.parseEther("1000"))
     await mockToken.transfer(user4.address, ethers.utils.parseEther("1000"))
     await mockToken.transfer(user5.address, ethers.utils.parseEther("1000"))
-    return {lottery, owner, user1, user2, user3, user4, user5, mockToken, team, vrf}
+    return {lottery, owner, user1, user2, user3, user4, user5, mockToken, team, vrf, upkeep}
   }
 
   async function setupStarted () {
     const init = await setup()
-    const {lottery, owner, user1, user2, user3, user4, user5, mockToken} = init
+    const {lottery, owner, user1, user2, user3, user4, user5, mockToken, upkeep} = init
     await lottery.activateLottery(parseEther("10"), await time.latest() + 3600)
+    await lottery.setUpkeeper(upkeep.address, true)
     await mockToken.connect(user1).approve(lottery.address, parseEther("1000"))
     await mockToken.connect(user2).approve(lottery.address, parseEther("1000"))
     await mockToken.connect(user3).approve(lottery.address, parseEther("1000"))
@@ -45,6 +46,26 @@ describe("Lottery", function () {
       await lottery.setPrice( parseEther("12"), 1, )
       const round1Info = await lottery.roundInfo(1)
       expect(round1Info.price).to.equal(parseEther("12"))
+    })
+    it("Should set a keeper address", async() => {
+      const { lottery, owner, user1, user2, user3, user4, user5, mockToken} = await loadFixture(setup);
+      await expect(lottery.connect(user1).setUpkeeper(user1.address, true)).to.be.revertedWith("Ownable: caller is not the owner");
+      await lottery.setUpkeeper(user1.address, true)
+      expect(await lottery.upkeeper(user1.address)).to.equal(true)
+      expect(await lottery.upkeeper(user2.address)).to.equal(false)
+    })
+    it("Should add an amount to the pot", async () => {
+      const { lottery, owner, user1, user2, user3, user4, user5, mockToken} = await loadFixture(setup);
+      await mockToken.approve(lottery.address, parseEther("1000"))
+      await expect(lottery.addToPot(parseEther("10"),0)).to.be.revertedWithCustomError(lottery, "BlazeLot__InvalidRound");
+      await lottery.addToPot(parseEther("10"),1)
+      await lottery.addToPot(parseEther("20"), 2)
+      expect((await lottery.roundInfo(1)).pot).to.equal(parseEther("10"))
+      expect((await lottery.roundInfo(2)).pot).to.equal(parseEther("20"))
+
+      await mockToken.connect(user1).approve(lottery.address, parseEther("1000"))
+      await lottery.connect(user1).addToPot(parseEther("30"), 1)
+      expect((await lottery.roundInfo(1)).pot).to.equal(parseEther("40"))
     })
 
   })
@@ -73,7 +94,7 @@ describe("Lottery", function () {
       ticketToBuy = convertToHex([88,1,0,3,0])
       expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(0)
 
-      // TODO THIS PARTICULAR CASE IS STILL PENDING
+      // use only numbers between 0 and 63
       ticketToBuy = convertToHex([1,40,63, 74, 148 ])
       expect(await lottery.checkTicketMatching(ticketWinner, ticketToBuy)).to.equal(1)
     })
@@ -100,9 +121,13 @@ describe("Lottery", function () {
       await lottery.connect(user4).buyTickets(new Array(40).fill(ticketToBuy))
       await lottery.connect(user5).buyTickets(new Array(60).fill(ticketToBuy))
 
+      expect((await lottery.roundInfo(0)).pot).to.equal(0)
+      expect((await lottery.roundInfo(1)).pot).to.equal(parseEther("10").mul(10 + 100 + 20 + 40 + 60))
+      expect((await lottery.roundInfo(2)).pot).to.equal(0)
+      // Nothing on ROUND 0
       let user1Tickets = await lottery.getUserTickets(user1.address, 0)
       expect(user1Tickets[2]).to.equal(0)
-      
+      // Normal rounds
       user1Tickets = await lottery.getUserTickets(user1.address, 1)
       expect(user1Tickets[2]).to.equal(10)
       user1Tickets = await lottery.getUserTickets(user2.address, 1)
@@ -114,5 +139,12 @@ describe("Lottery", function () {
       user1Tickets = await lottery.getUserTickets(user5.address, 1)
       expect(user1Tickets[2]).to.equal(60)
     })
+    it("Should not allow to buy tickets if round time has ended", async ()=>{
+      const { lottery, owner, user1, user2, user3, user4, user5} = await loadFixture(setupStarted);
+      const ticketToBuy = convertToHex([10,20,30,40,50])
+      await time.increase(3601)
+      await expect(lottery.connect(user1).buyTickets(new Array(10).fill(ticketToBuy))).to.be.revertedWithCustomError(lottery,"BlazeLot__RoundInactive").withArgs(1)
+    })
   })
+
 })
