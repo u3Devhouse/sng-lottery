f35c059e78dd3f629aa131615d8e7e056164c32c
Semi Invader <=>
2023-06-29 22:36:42 -0600

    dark gold is primary

--- ui-blaze-game/tailwind.config.js
+++ ui-blaze-game/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     themes:[
       {
         'mytheme':{
+          'primary': "#8F763B",
           'accent': "#F94F59",
           'secondary': "#E0B654" 
         }
@@ -20,7 +21,8 @@ module.exports = {
         'primary-bg': "#27072A",
         'secondary-bg' : "#1D152B",
         'primary-text': "#D6B159",
-        'golden': "#E0B654"
+        'golden': "#E0B654",
+        'golden-dark': "#8F763B",
       },
       fontFamily:{
         'outfit': ['var(--font-outfit)'],
