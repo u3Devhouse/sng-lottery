e158a4fee15203a2ab93710062ccf8f5c792dee3
Semi Invader <=>
2023-07-16 21:40:49 +0100

    if iframe show link to direct site

--- ui-blaze-game/src/app/page.tsx
+++ ui-blaze-game/src/app/page.tsx
@@ -4,8 +4,22 @@ import PastRounds from "@/components/lottery/PastRounds";
 import OwnerCard from "@/components/owner/OwnerCard";
 
 const Page = () => {
+  const isIframe = typeof window !== "undefined" && window !== window.top;
   return (
     <>
+      {isIframe && (
+        <section className="lg:hidden pb-12">
+          <div className="flex flex-col items-center justify-center">
+            <a
+              className="link text-2xl"
+              href="https://blaze-lottery.vercel.app/"
+              target="_parent"
+            >
+              Mobile users use THIS site
+            </a>
+          </div>
+        </section>
+      )}
       <section className="w-full pb-4 flex flex-col items-center">
         <h2 className="italic font-outfit pb-4 text-2xl font-bold">
           Current Round
--- ui-blaze-game/src/components/layout/Header.tsx
+++ ui-blaze-game/src/components/layout/Header.tsx
@@ -1,17 +1,14 @@
-import Link from "next/link";
 import Image from "next/image";
-import { TiSocialFacebook, TiSocialTwitter } from "react-icons/ti";
-import { GrInstagram } from "react-icons/gr";
-import { RiMessengerFill } from "react-icons/ri";
-import { HiMenuAlt2 } from "react-icons/hi";
 import { Web3Button } from "@/components/wagmiComponents/Web3Button";
 import jackpotImg from "../../../public/jackpot.png";
+import classNames from "classnames";
 
 const Header = () => {
+  const isIframe = typeof window !== "undefined" && window !== window.top;
   return (
     <header className="flex flex-col items-center justify-between py-12 gap-4">
       <Image src={jackpotImg} alt="Jackpot logo" />
-      <div className="pt-4">
+      <div className={classNames("pt-4", isIframe && "hidden")}>
         <Web3Button />
       </div>
     </header>
