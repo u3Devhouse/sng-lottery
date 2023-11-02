817bf99b6ba942b9f50a268ccaf93bc30c9146ff
Semi Invader <=>
2023-11-02 08:10:54 -0600

    update packages and clean up dev

--- ui-blaze-game/next.config.js
+++ ui-blaze-game/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false }
+  webpack: (config, context) => {
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding|supports-color)$/,
+        }),
+      )
+    }
     return config
   },
 }
