124323c6ae311cf88628fcbca257bd2241e84f89
Semi Invader <41244007+jmanywhere@users.noreply.github.com>
2024-07-08 20:41:21 -0600

    ;init of tests

--- foundry-lottery/test/Lottery.t.sol
+++ foundry-lottery/test/Lottery.t.sol
@@ -3,6 +3,28 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 
+// THESE TESTS ARE to be MADE IN  BSC MAINNET
 contract LotteryTest is Test {
-    function setUp() public {}
+    address usdcWhale = 0xadd;
+    address sngWhale = 0xadd;
+
+    address upkeeper = makeAddr("upkeeper");
+
+    function setUp() public {
+        // set wallets that hold USDC & SNG
+        // Create vrf coordinator mock
+        // coordinator creates subscription
+        // coordinator funds subscription
+        // DEPLOY LOTTERY
+        // add lottery as a consumer
+    }
+
+    function activateLottery() public {
+        // buy tickets
+        // check if tickets are bought
+    }
+
+    modifier lotteryActivated() {
+        _;
+    }
 }
