6edb021086b5450c55c73e1d43c41a59937ef675
Semi Invader <41244007+jmanywhere@users.noreply.github.com>
2024-07-12 12:01:52 +0100

    changed header

--- ui-blaze-game/src/components/layout/Header.tsx
+++ ui-blaze-game/src/components/layout/Header.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import { IframeWeb3Button } from "@/components/wagmiComponents/Web3Button";
-import jackpotImg from "../../../public/jackpot.png";
+import jackpotImg from "@/../public/assets/SNG Jackpot 1.svg";
 
 const Header = () => {
   return (
-    <header className="flex flex-col items-center justify-between py-12 gap-4">
+    <header className="flex flex-col items-center justify-between py-12 gap-4 border-t-2 border-[#E30613]">
       <Image src={jackpotImg} alt="Jackpot logo" />
       <IframeWeb3Button />
     </header>
